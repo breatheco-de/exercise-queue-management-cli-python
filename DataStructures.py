8691e88b8478e5ebc74b75c0088bdb9c6eb5eb2c
Alejandro Sanchez <aalejo@gmail.com>
2020-01-19 19:39:12 +0000

    s

--- DataStructures.py
+++ DataStructures.py
@@ -1,18 +1,20 @@
 class Queue:
 
-    def __init__(self, mode):
-        self._queue = []
+    def __init__(self, mode, current_queue=[]):
+        self.queue = current_queue
         # depending on the _mode, the queue has to behave like a FIFO or LIFO
         if mode is None:
-            raise "Please specify a queue mode FIFO or LIFO"
+            raise "Please specify a queue mode = FIFO or LIFO"
         else:
-            self._mode = mode
+            self.mode = mode
     
     def enqueue(self, item):
-        pass
+        if self.mode == 'FIFO':
+            return self.queue.append(item)
     def dequeue(self):
-        pass
+        if self.mode == 'FIFO':
+            return self.queue.pop(0)
     def get_queue(self):
-        pass
+        return self.queue
     def size(self):
-        return len(self._queue) 
+        return len(self.queue) 
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ if sys.version_info[0] < 3:
     print("Error: You must use Python 3, try running $ python3 app.py or updating the python interpreter")
 
 #there exercise code starts here
+import json
 from DataStructures import Queue
 
 # there queue has to be declared globally (outside any other function)
@@ -12,20 +13,50 @@ queue = Queue(mode="FIFO")
 
 def show_main_menu():
     print('''
-        What would you like to do (type a number and the enter key)?
-            - Type 1: For adding someone to the Queue.
-            - Type 2: For removing someone from the Queue.
-            - Type 3: For printing the current Queue state.
-            - Type 4: To export the queue to a JSON file.
-            - Type 5: To quit
+What would you like to do (type a number and the enter key)?
+    - Type 1: For adding someone to the Queue.
+    - Type 2: For removing someone from the Queue.
+    - Type 3: For printing the current Queue state.
+    - Type 4: To export the queue to the queue.json file.
+    - Type 5: To import the queue from the queue.json file.
+    - Type 6: To quit
     ''')
     response = input()
     return response
     
+def add_to_queue():
+    print("Type the name you want to add into the list")
+    name = input()
+    queue.enqueue(name)
+    print(name+" was added to the list")
+
+def remove_from_queue():
+    name = queue.dequeue()
+    print("It is "+name+" turn, therefore he was removed from the list")
+
 def print_queue():
     # you must print on the console the entire queue list
-    pass
+    print("Printing the entire list...")
+    print(queue.get_queue())
+
+
+def export_queue():
+    # you must print on the console the entire queue list
+    file = open('queue.json', 'w+') # open the file for writing 'w', create if it doesn't exists
+    file.write(json.dumps(queue.get_queue())) # write the content
+    file.close() # close the file
+    print("List exported successfully, open the queue.json file to review it.")
+
+def import_queue():
+    # you must print on the console the entire queue list
+    print("Importing the entire list...")
+    filePointer = open("queue.json", "r") 
+    data = json.load(filePointer)
     
+    global queue # <-- we have to tell python we are referring to the global variable queue
+    queue = Queue(mode="FIFO", current_queue=data)
+    print("List import successfully, print the list to review the updates.")
+        
 def start():
     
     print("Hello, this is the Command Line Interface for a Queue Managment application.");
@@ -37,11 +68,23 @@ def start():
             option = int(option)
         except ValueError:
             print("Invalid option "+str(option))
+
+        # add your options here using conditionals (if)
+        if option == 1:
+            add_to_queue()
+        elif option == 2:
+            remove_from_queue()
+        elif option == 3:
+            print_queue()
+        elif option == 4:
+            export_queue()
+        elif option == 5:
+            import_queue()
         
         # now, lets intepret the option
-        if option > 5:
+        if option > 6:
             print("Invalid option "+str(option))
-        elif option == 5:
+        elif option == 6:
             print("Bye bye!")
             return None
 
